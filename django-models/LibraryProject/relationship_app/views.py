fcb541ba3e2e0d0234e66c63ee4b616bd7a0b73b
Michael Kyulu <michaelkyulu4@gmail.com>
2025-09-15 21:14:59 +0300

    Edit view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from .models import Book
 # Function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 # Class-based view
