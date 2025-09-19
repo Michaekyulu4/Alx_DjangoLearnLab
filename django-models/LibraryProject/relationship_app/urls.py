6293b13bc19de890e5b57b4be3ad1eddbe064581
Michael Kyulu <michaelkyulu4@gmail.com>
2025-09-19 07:43:17 +0300

    Admin view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -34,7 +34,5 @@ urlpatterns = [
     path("admin-dashboard/", views.admin_view, name="admin_view"),
     path("librarian-dashboard/", views.librarian_view, name="librarian_view"),
     path("member-dashboard/", views.member_view, name="member_view"),
+    path("admin-view/", views.admin_view, name="admin_view"),
 ]
-
-# relationship_app/urls.py
-  
