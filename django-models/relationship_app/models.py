bbb0b175bdfdcb0b095be5228e5ba0e9000d45b4
Michael Kyulu <michaelkyulu4@gmail.com>
2025-09-20 11:08:48 +0300

    Adding meta

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
-from django.db import models
-
 # Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -13,13 +10,23 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-# Book model
+
+# Book model (merged into ONE definition)
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
     def __str__(self):
         return f"{self.title} by {self.author.name}"
+
 
 # Library model
 class Library(models.Model):
@@ -29,6 +36,7 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+
 # Librarian model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
@@ -36,9 +44,9 @@ class Librarian(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.library.name})"
-    
 
 
+# UserProfile model
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ("Admin", "Admin"),
@@ -60,19 +68,3 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     else:
         instance.userprofile.save()
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey("Author", on_delete=models.CASCADE)
-    published_date = models.DateField()
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
-
-    def __str__(self):
-        return self.title
-# relationship_app/urls.py
