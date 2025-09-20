aa86f6ba8c70335c6fc4d4250d7df1c8300f200b
Michael Kyulu <michaelkyulu4@gmail.com>
2025-09-20 12:05:11 +0300

    AddinList_book

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -33,11 +33,15 @@ def edit_book(request, book_id):
     return render(request, "bookshelf/edit_book.html", {"book": book})
 
 # Delete a book (requires can_delete)
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/list_books.html", {"books": books})
+
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == "POST":
         book.delete()
         return redirect("list_books")
-    return render(request, "bookshelf/delete_book.html", {"book": book})
-# Create your views here.
+    return render(request, "bookshelf/confirm_delete.html", {"book": book})
