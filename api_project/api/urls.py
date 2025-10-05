0b203f42252cdda3c9f938e0a360b507c43f9f1e
Michael Kyulu <michaelkyulu4@gmail.com>
2025-10-05 13:40:49 +0300

    Adding default router to urls

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
 
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 urlpatterns = [
+    # Route for the ListAPIView (previous task)
     path('books/', BookList.as_view(), name='book-list'),
-     path('', include(router.urls)),
+
+    # Include automatically generated CRUD routes from the router
+    path('', include(router.urls)),
 ]
